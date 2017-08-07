ec7ae89bc05d6c4d3ec6a4a7509bd87be67ea1fe
Utku Ufuk <utkuufuk@gmail.com>
2017-08-07 20:15:31 +0300

    polish multi-variate linear regression cost function

--- Assignments/ex1/computeCostMulti.m
+++ Assignments/ex1/computeCostMulti.m
@@ -1,10 +1,8 @@
 function J = computeCostMulti(X, y, theta)
-%COMPUTECOSTMULTI Compute cost for linear regression with multiple variables
-%   J = COMPUTECOSTMULTI(X, y, theta) computes the cost of using theta as the
-%   parameter for linear regression to fit the data points in X and y
+%Computes cost for multi-variate linear regression
 
-    m = length(y);                              % number of training examples
+    numExamples = length(y);
     predictions = X * theta;
     squaredErrors = (predictions - y) .^ 2;
-    J = 1 / (2 * m) * sum(squaredErrors);
+    J = sum(squaredErrors) / (2 * numExamples);
 end
