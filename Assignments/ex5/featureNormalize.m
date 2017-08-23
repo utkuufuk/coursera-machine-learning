b7fc1b8659595e48997041668e5ea306b81373a1
utkuufuk <utkuufuk@gmail.com>
2017-08-23 17:14:15 +0300

    bugfix in feature normalization function

--- Assignments/ex5/featureNormalize.m
+++ Assignments/ex5/featureNormalize.m
@@ -2,6 +2,6 @@ function [X, means, stDevs] = featureNormalize(X)
 % Normalizes X where the mean of each feature is 0 and the standard deviation is 1.
     means = mean(X);
     stDevs = std(X);
-    X = X - bsxfun(@minus, X, means);       % subtract the mean
-    X = X - bsxfun(@rdivide, X, stDevs);    % divide by the standard deviation
+    X = bsxfun(@minus, X, means);       % subtract the mean
+    X = bsxfun(@rdivide, X, stDevs);    % divide by the standard deviation
 end
