1d9f62cd88b027dca2ba654b8899d41bfee6c31a
utkuufuk <utkuufuk@gmail.com>
2017-08-23 11:50:57 +0300

    polish feature normalization function

--- Assignments/ex1/ex1_multi.m
+++ Assignments/ex1/ex1_multi.m
@@ -4,13 +4,13 @@
 data = load('ex1data2.txt');
 X = data(:, 1:2);
 y = data(:, 3);
-m = length(y);
+numExamples = length(y);
 
 % Scale features and set them to zero mean
-[X, mu, sigma] = featureNormalize(X);
+[X, means, stDevs] = featureNormalize(X);
 
 % Add intercept term to X
-X = [ones(m, 1), X];
+X = [ones(numExamples, 1), X];
 
 % Choose some alpha value
 alpha = 0.2;
@@ -18,11 +18,12 @@ num_iters = 100;
 
 % Init Theta and Run Gradient Descent 
 theta = zeros(3, 1);
-[theta, J_history] = gradientDescent(X, y, theta, alpha, 0, num_iters);
+[theta, costHistory] = gradientDescent(X, y, theta, alpha, 0, num_iters);
+fprintf('The final cost is: %f\n', costHistory(end));
 
 % Plot the convergence graph
 figure;
-plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
+plot(1:numel(costHistory), costHistory, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
 
@@ -32,8 +33,8 @@ fprintf(' %f \n', theta);
 fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
-normalizedFeature1 = (1650 - mu(1)) / sigma(1);
-normalizedFeature2 = (3 - mu(2)) / sigma(2);
+normalizedFeature1 = (1650 - means(1)) / stDevs(1);
+normalizedFeature2 = (3 - means(2)) / stDevs(2);
 price = [1, normalizedFeature1, normalizedFeature2] * theta;
 fprintf('Predicted price of a 1650 sq-ft, 3 br house:\n $%f\n', price);
 
@@ -41,10 +42,10 @@ fprintf('Predicted price of a 1650 sq-ft, 3 br house:\n $%f\n', price);
 data = csvread('ex1data2.txt');
 X = data(:, 1:2);
 y = data(:, 3);
-m = length(y);
+numExamples = length(y);
 
 % Add intercept term to X
-X = [ones(m, 1) X];
+X = [ones(numExamples, 1) X];
 
 % Calculate the parameters from the normal equation
 theta = normalEquation(X, y);
--- Assignments/ex1/featureNormalize.m
+++ Assignments/ex1/featureNormalize.m
@@ -1,8 +1,8 @@
-function [xNorm, m, sigma] = featureNormalize(X)
+function [X, means, stDevs] = featureNormalize(X)
 % Returns a normalized version of X where the mean value of each feature is 0 and the standard deviation is 1.
     
-    m = mean(X);
-    xNorm = X - m(ones(size(X, 1), 1), :);
-    sigma = std(X);
-    xNorm = xNorm ./ sigma(ones(size(X, 1), 1), :);
+    means = mean(X);
+    X = X - means(ones(size(X, 1), 1), :);
+    stDevs = std(X);
+    X = X ./ stDevs(ones(size(X, 1), 1), :);
 end
